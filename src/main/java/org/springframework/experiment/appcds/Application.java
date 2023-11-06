539a0f875f722db511aefbac7070c98242996e7b
Stéphane Nicoll <snicoll@vmware.com>
2023-11-06 09:25:31 +0100

    Initial commit

---
+++ src/main/java/org/springframework/experiment/appcds/Application.java
@@ -0,0 +1,11 @@
+package org.springframework.experiment.appcds;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
